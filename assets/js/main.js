bb87fc408523a65a5e94563738c79bce62ed334f
SalvoBevilacqua <salvatorebevilacqua91@gmail.com>
2025-02-17 00:06:08 +0100

    wip

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
-
+    convertDate();
 });
+
+// per modificare il formato di ritorno delle date
+function convertDate() {
+    const inputs = document.querySelectorAll('[type="date"]');
+    inputs.forEach(input => {
+        input.addEventListener("change", function () {
+            let date = new Date(this.value);
+            if (!isNaN(date.getTime())) {
+                let formattedDate = date.toISOString().split("T")[0];
+                this.value = formattedDate;
+            }
+        });
+    });
+}
